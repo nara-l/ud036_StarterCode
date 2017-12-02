ad4db418f8a7b03896b799b066a225f151337898
Nara Lawrence <njielitumbe@gmail.com>
2017-12-02 11:47:43 -0500

    Cleaned code and added some comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,55 +5,50 @@ import tmdbsimple as tmdb
 from unidecode import unidecode
 
 
-''' file create movies to be loaded from an instance of Media class '''
-
-#method to convert movie name to snake case https://stackoverflow.com/a/1176023/851056
-def convert(name):
-    new_name = name.replace(" ", "").replace("'","")
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', new_name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-    
+''' file create movies to be loaded from an instance of Media class. Movie details are from TMDB api '''
+   
 def remove_non_ascii(text):
     return text.encode("utf-8")
-    
-# Create list of movies here. Add as many as you wish
+       
+# create list of movies here. Add as many as you wish
 list_of_movies = ["The Devil's Advocate", "Inception",  "Alfie", "Temple Grandin", "American Hustle", "Lord of The Rings", "Death Note", "Batman Begins", "Gone Girl"]
 
+# start using tmdb api
 tmdb.API_KEY = 'a2c2494f6db6d934edc2563b347047bb'
-
 j = 0
 count = len(list_of_movies)
-convert(list_of_movies[j])
 build_movies = []
-             
-
+            
 while j < count :
 
         search = tmdb.Search()
         response = search.movie(query=list_of_movies[j])
         s = search.results[0]
         
-        #get youtube trailers at once without making a second call https://github.com/celiao/tmdbsimple/issues/6
+        # get youtube trailers at once without making a second call https://github.com/celiao/tmdbsimple/issues/6
         kwargs = {'append_to_response': 'trailers'}
         movie_ = tmdb.Movies(s['id']).info(**kwargs)
         video_ = movie_.get('trailers')
         
-        #to eliminate indexError
+        # to eliminate indexError
         for index, value in enumerate(video_['youtube']):
             if value is not 0:
                 vid_source = video_['youtube'][index]['source']
         
-        movie_name = convert(list_of_movies[j])
-        video = "https://www.youtube.com/watch?v=" + vid_source
+        # get variables needed for media class
+        trailer_youtube_url = "https://www.youtube.com/watch?v=" + vid_source
         title =  list_of_movies[j]
         poster_image_url =  'http://cf2.imgobject.com/t/p/original' + s['poster_path']
-        trailer_youtube_url =  video
         
+        # create an instance of media class for every movie
         i = media.Movies(remove_non_ascii(title), remove_non_ascii(poster_image_url), remove_non_ascii(trailer_youtube_url))
-        #print i #out puts as it should
+        
+        # populate list for fresh tomatoes
         build_movies.append(i)
 
         j += 1
 
-print build_movies #outputs with extra quotation marks which causes method below to throw an error
+#print build_movies
+
+# render page
 fresh_tomatoes.open_movies_page(build_movies)
--- media.py
+++ media.py
@@ -5,15 +5,3 @@ class Movies():
         self.title = movie_title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        
-    # For call to repr(). Prints object's information
-    def __repr__(self):
-        return '[%s, %s, %s]' % (self.title, self.poster_image_url, self.trailer_youtube_url)    
- 
-    # For call to str(). Prints readable form
-    def __str__(self):
-       return '[%s, %s, %s]' % (self.title, self.poster_image_url, self.trailer_youtube_url)   
-       
-       
-    __repr__ = __str__
-        
